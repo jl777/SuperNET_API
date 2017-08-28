e55fb800e5c847fab0e507462142ceeb72a9a856
Wladimir J. van der Laan <31405203+laanwj2@users.noreply.github.com>
2017-08-28 11:26:30 +0100

    Update exchangepairs.h

--- plugins/InstantDEX/deprecated/exchangepairs.h
+++ plugins/InstantDEX/deprecated/exchangepairs.h
@@ -2,7 +2,7 @@
 //  exchangepairs.h
 //
 //  Created by jl777 on 13/4/15.
-//  Copyright (c) 2015 jl777. All rights reserved.
+//  Copyright (c) 2017 jl777. All rights reserved.
 //
 
 #ifndef xcode_exchangepairs_h
